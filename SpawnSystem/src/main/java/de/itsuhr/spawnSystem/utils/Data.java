dd590c965f577d26d0df1ea7af1b27654289cd00
ItsUhr <148636607+ItsUhr@users.noreply.github.com>
2024-07-27 14:14:04 +0200

    Add files via upload

---
+++ SpawnSystem/src/main/java/de/itsuhr/spawnSystem/utils/Data.java
@@ -0,0 +1,38 @@
+package de.itsuhr.spawnSystem.utils;
+
+public class Data {
+
+    public String prefix;
+    public String consoleSender;
+    public String usage;
+    public String noPerms;
+
+    public boolean soundYes;
+    public boolean soundNo;
+    public boolean sendActionBar;
+
+    public String setSpawnPermission;
+    public String spawnPermission;
+    public String spawnReloadPermission;
+
+    public String world;
+    public double x;
+    public double y;
+    public double z;
+    public float yaw;
+    public float pitch;
+
+    public String setSpawnMessage;
+    public String spawnTeleportMessage;
+
+    public int countdown;
+    public String countdown_message;
+
+    public String spawnNotSet;
+
+    public boolean playerRespawnSpawn;
+    public boolean playerFirstJoinSpawn;
+    public boolean playerAlwaysJoinSpawn;
+
+    public String reloadMessage;
+}
